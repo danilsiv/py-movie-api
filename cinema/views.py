b94d82f7ccee8be5a6a0735f091b80d12bbe8d0e
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-12-30 11:25:42 +0200

    Create movie_list_view

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def movie_list_view(request) -> Response:
+    if request.method == "GET":
+        movie_list = Movie.objects.all()
+        serializer = MovieSerializer(movie_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
