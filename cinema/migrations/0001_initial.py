5291acf12b7bf180d73732d95468656017d9c382
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-02 18:16:55 +0200

    Format all files with black

--- cinema/migrations/0001_initial.py
+++ cinema/migrations/0001_initial.py
@@ -7,17 +7,27 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Movie',
+            name="Movie",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('description', models.TextField()),
-                ('duration', models.PositiveIntegerField(help_text='duration in minutes')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("description", models.TextField()),
+                (
+                    "duration",
+                    models.PositiveIntegerField(help_text="duration in minutes"),
+                ),
             ],
         ),
     ]
--- cinema/urls.py
+++ cinema/urls.py
@@ -4,7 +4,7 @@ from cinema.views import movie_list_view, movie_detail_view
 
 urlpatterns = [
     path("movies/", movie_list_view, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail_view)
+    path("movies/<int:pk>/", movie_detail_view, name="movie-detail"),
 ]
 
 
